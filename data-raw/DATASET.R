cf55b634247b8257d37f03d0373de072b7e34d7d
HunterGleason <Hunter.Gleason@gov.bc.ca>
2022-02-07 11:35:49 -0800

    external data try 5

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,6 +4,7 @@ exampleData <- stars::read_stars("data-raw/ccdc_img_expl.tif",n_proxy=10^10)
 # Save the cleaned data in the required R package location
 usethis::use_data(exampleData,overwrite = T)
 
-landcover <- terra::vect("data-raw/zonePolyExmpl.shp")
+landcover <- sf::read_sf("data-raw/zonePolyExmpl.shp")
 
 usethis::use_data(landcover,overwrite = T)
+
