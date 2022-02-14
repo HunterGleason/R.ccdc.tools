4dc897a950fb8a0f2ad9d67a975197cfe70630c9
HunterGleason <Hunter.Gleason@gov.bc.ca>
2022-02-14 09:26:54 -0800

    added function for getting CCDC coef image at a specified Julian date

--- R/get_ccdc_current_seg.R
+++ R/get_ccdc_current_seg.R
@@ -129,3 +129,88 @@ gen_latest_ccdc_rast<-function(ccdc_img,max_seg,as_SpatRaster=T)
   }
   
 }
+
+
+#' Get the segment coefficients from the CCDC image at a specific Julian date (see Zhu & Woodcock 2014) 
+#'
+#' For pixel locations where change has occurred (number of segments >1) this function returns 
+#' the CCDC model coefficients associated with the segment that corresponds in time with the specified Julian date,
+#' as well as the start date of the segment as a Julian date. If there is only one segment (no change), returns coefficients 
+#' for the that segment. !!Assumes that up to four segments may exist (see Earth Engine script)!!
+#'
+#' @param ccdc_img (SpatRaster, stars) The CCDC image for which to extract coefficients.
+#' @param jdoy (integer) Julian date for which to extract CCDC coefficients. 
+#' @return (SpatRaster) A SpatRaster with all CCDC coefficients (and tStart,tEnd) associated with the segment corresponding with the
+#' specified Julian date. 
+#' @export
+gen_ccdc_at_jdoy<-function(ccdc_img,jdoy)
+{
+  ccdc_img<-terra::as.data.frame(ccdc_img,xy=T)
+  
+  Rcpp::cppFunction('NumericVector get_seg_at_jdoy(int jdoy,NumericVector s1tStart,NumericVector s1tEnd,NumericVector s2tStart,NumericVector s2tEnd,NumericVector s3tStart,NumericVector s3tEnd,NumericVector s4tStart,NumericVector s4tEnd) {
+  
+  int n = s1tStart.size();
+  NumericVector out(n);
+  
+  for(int i = 0; i < n; ++i){
+    if(jdoy >= s1tStart[i] && jdoy <= s1tEnd[i])
+            {
+            out[i]=1;
+            }else if (jdoy >= s2tStart[i] && jdoy <= s2tEnd[i])
+            {
+            out[i]=2;
+            }else if (jdoy >= s3tStart[i] && jdoy <= s3tEnd[i])
+            {
+            out[i]=3;
+            }else if (jdoy >= s4tStart[i] && jdoy <= s4tEnd[i])
+            {
+            out[i]=4;
+            }
+  }
+  
+  return out;
+}')
+  
+  seg_idx<-get_seg_at_jdoy(jdoy,
+                           ccdc_img$S1_tStart,
+                           ccdc_img$S1_tEnd,
+                           ccdc_img$S2_tStart,
+                           ccdc_img$S2_tEnd,
+                           ccdc_img$S3_tStart,
+                           ccdc_img$S3_tEnd,
+                           ccdc_img$S4_tStart,
+                           ccdc_img$S4_tEnd)
+  
+  
+  s1_names_select<-c("x","y")
+  s2_names_select<-c("x","y")
+  s3_names_select<-c("x","y")
+  s4_names_select<-c("x","y")
+  names_final<-c("x","y")
+  
+  for(b in c("blue","green","red","nir","swir1","swir2"))
+  {
+    for(coef in c("INTP","SLP","COS","SIN","COS2","SIN2","COS3","SIN3"))
+    {
+      s1_names_select<-c(s1_names_select,paste0("S1_",b,"_coef_",coef),"S1_tStart","S1_tEnd")
+      s2_names_select<-c(s2_names_select,paste0("S2_",b,"_coef_",coef),"S2_tStart","S2_tEnd")
+      s3_names_select<-c(s3_names_select,paste0("S3_",b,"_coef_",coef),"S3_tStart","S3_tEnd")
+      s4_names_select<-c(s4_names_select,paste0("S4_",b,"_coef_",coef),"S4_tStart","S4_tEnd")
+      names_final<-c(names_final,paste0(b,"_",coef))
+    }
+  }
+  
+  
+  ccdc_img<-rbind(as.matrix(ccdc_img[seg_idx==1,s1_names_select]),
+                  as.matrix(ccdc_img[seg_idx==2,s2_names_select]),
+                  as.matrix(ccdc_img[seg_idx==3,s3_names_select]),
+                  as.matrix(ccdc_img[seg_idx==4,s4_names_select]))
+  
+  ccdc_img<-as.data.frame(ccdc_img)
+  
+  colnames(ccdc_img)<-c(names_final,"tStart","tEnd")
+  
+  ccdc_img<-terra::rast(ccdc_img,type="xyz")
+  
+  return(ccdc_img)
+}
--- data-raw/zonePolyExmpl-shp.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `zonePolyExmpl.shp` dataset goes here
-
-usethis::use_data(zonePolyExmpl.shp, overwrite = TRUE)
